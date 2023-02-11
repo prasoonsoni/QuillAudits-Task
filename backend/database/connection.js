3041ae0cf2d025928032aa1da213c7e18052483b
Prasoon Soni <prasoonsoni03@gmail.com>
2023-02-11 16:53:06 +0530

    ✨ Added MongoDB Connection

---
+++ backend/database/connection.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+dotenv.config()
+const { MONGO_URI } = process.env
+
+const connectToDatabase = () => {
+    try {
+        mongoose.connect(
+            MONGO_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                keepAlive: true
+            },
+            () => {
+                console.log('Mongoose is Connected.')
+            }
+        )
+    } catch (err) {
+        console.log(`Could not connect: ${err}`)
+    }
+    const dbConnection = mongoose.connection
+
+    dbConnection.on('error', (err) => {
+        console.log(`Connection Error: ${err}`)
+    })
+
+    dbConnection.once('open', () => {
+        console.log('Connected to DB!')
+    })
+}
+
+export default connectToDatabase
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-dotenv.config()
+import connectToDatabase from "./database/connection.js"
 
+dotenv.config()
 const app = express()
 const port = 5000 || process.env.PORT
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+connectToDatabase()
 
 app.get('/', (req, res) => {
     res.send("QuillAudits Backend Working")

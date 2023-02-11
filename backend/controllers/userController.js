546610f4eef1caf783716bd88c6c0445ad993984
Prasoon Soni <prasoonsoni03@gmail.com>
2023-02-11 18:10:58 +0530

    🚧 Added route to Login User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,4 +26,38 @@ const signup = async (req, res) => {
     }
 }
 
-export default { signup }
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.json({ success: false, message: 'User Not Found.' })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.json({ success: false, message: 'Incorrect Password.' })
+        }
+        const data = { user: { _id: user._id } }
+        const token = jwt.sign(data, process.env.JWT_SECRET)
+        return res.json({ success: true, message: 'User Logged In Successfully.', token: token })
+    } catch (err) {
+        console.log(err.message)
+        res.json({ success: false, message: "Some Internal Server Error Occured" })
+
+    }
+}
+
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.user._id }).select('-password')
+        if (!user) {
+            return res.json({ success: false, message: 'User Not Found.' })
+        }
+        return res.json({ success: true, message: 'User Found Successfully.', user })
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: 'Some Internal Server Error Occured.' })
+    }
+}
+
+export default { signup, login, getUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from "express"
 import userController from "../controllers/userController.js"
+import fetchUser from "../middleware/fetchUser.js"
 const router = express.Router()
 
+router.get('/', fetchUser, userController.getUser)
 router.post('/signup', userController.signup)
+router.post('/login', userController.login)
 
 export default router

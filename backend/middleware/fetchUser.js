ab566a554e89fe8444d95383eca1334362dfc086
Prasoon Soni <prasoonsoni03@gmail.com>
2023-02-11 18:02:15 +0530

    🚧 Added Middleware to Fetch User

---
+++ backend/middleware/fetchUser.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+dotenv.config()
+
+const fetchUser = async (req, res, next) => {
+    const token = req.header('auth-token')
+    if (!token) {
+        return res.json({ success: false, message: 'Authentication token is required.' })
+    }
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = data.user
+        next()
+    } catch (error) {
+        res.json({ success: false, message: 'Authentication token is not valid.' })
+        console.log(error.message)
+    }
+}
+
+export default fetchUser

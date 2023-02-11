59897ba1ff1ffa141ae3307e94dd20a1fab239ee
Prasoon Soni <prasoonsoni03@gmail.com>
2023-02-11 17:47:22 +0530

    ✨ Added User Model

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.model('User', UserSchema)

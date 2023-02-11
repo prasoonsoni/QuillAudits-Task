b656626d433a6d6d80c7b7c4ce1af9d408e96766
Prasoon Soni <prasoonsoni03@gmail.com>
2023-02-11 21:08:11 +0530

    💄 Added Hero Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './App';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import theme from './config/theme';
+import Hero from './pages/Hero'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Hero />,
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );

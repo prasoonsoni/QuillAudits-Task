bcea34c4dc62bfce5a9e56db30266afe832f506b
Prasoon Soni <prasoonsoni03@gmail.com>
2023-02-12 21:29:03 +0530

    🚧 Updated CORS

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = 5000 || process.env.PORT
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: ["http://localhost:3000", "https://quill-audits-frontend.vercel.app"]
     }
 });
 
